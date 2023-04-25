5376ec478d613b17e91a3a3632191729b12c00f0
Abanim2 <115773506+Abanim2@users.noreply.github.com>
2023-04-25 12:27:06 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,58 @@
 
+// const express = require('express');
+// const app = express()
+
+
+// // Your code goes here
+
+
+// module.exports = app;
+
+
+
+
 const express = require('express');
 const app = express()
+const mongoose = require("mongoose");
+const Subscriber = require("./models/subscribers");
 
+app.use(express.json());
 
-// Your code goes here
+app.get("/subscribers", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.status(200).json(subscribers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
+app.get("/subscribers/names", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find().select("name subscribedChannel");
+    res.status(200).json(subscribers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(400).json({ message: "Invalid Subscriber ID" });
+    }
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 
+  res.subscriber = subscriber;
+  next();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/subscribers/:id", getSubscriber, (req, res) => {
+  res.json(res.subscriber);
+});
 
 module.exports = app;
